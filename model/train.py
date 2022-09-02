c01ed5bac668e5924c202fc3786ad485ee068bf5
Karan <karanpargal007@gmail.com>
2022-09-03 02:35:17 +0530

    :sparkles: Add model

---
+++ model/train.py
@@ -0,0 +1,139 @@
+from keras.preprocessing.image import img_to_array,load_img
+from os import listdir
+from os.path import isfile, join, isdir
+import numpy as np
+from PIL import Image
+import keras
+from keras.layers import Conv2DTranspose, ConvLSTM2D, BatchNormalization, TimeDistributed, Conv2D
+from keras.models import Sequential, load_model
+from keras_layer_normalization import LayerNormalization
+import matplotlib.pyplot as plt
+
+train_dataset_path = './datasets/UCSD_Dataset/UCSDped1/Train'
+test_dataset_path = './datasets/UCSD_Dataset/UCSDped1/Test'
+BATCH_SIZE = 4
+EPOCHS = 3
+MODEL_PATH = './model.h5'
+
+def get_clips_by_stride(stride, frames_list, sequence_size):
+    """ For data augmenting purposes.
+    Parameters
+    ----------
+    stride : int
+        The distance between two consecutive frames
+    frames_list : list
+        A list of sorted frames of shape 256 X 256
+    sequence_size: int
+        The size of the lstm sequence
+    Returns
+    -------
+    list
+        A list of clips , 10 frames each
+    """
+    clips = []
+    sz = len(frames_list)
+    clip = np.zeros(shape=(sequence_size, 256, 256, 1))
+    cnt = 0
+    for start in range(0, stride):
+        for i in range(start, sz, stride):
+            clip[cnt, :, :, 0] = frames_list[i]
+            cnt = cnt + 1
+            if cnt == sequence_size:
+                clips.append(np.copy(clip))
+                cnt = 0
+    return clips
+
+def get_training_set():
+    """
+    Returns
+    -------
+    list
+        A list of training sequences of shape (NUMBER_OF_SEQUENCES,SINGLE_SEQUENCE_SIZE,FRAME_WIDTH,FRAME_HEIGHT,1)
+    """
+    clips = []
+    for f in sorted(listdir(train_dataset_path)):
+        directory_path = join(train_dataset_path, f)
+        if isdir(directory_path):
+            all_frames = []
+            for c in sorted(listdir(directory_path)):
+                img_path = join(directory_path, c)
+                if str(img_path)[-3:] == "tif":
+                    img = Image.open(img_path).resize((256, 256))
+                    img = np.array(img, dtype=np.float32) / 256.0
+                    all_frames.append(img)
+            for stride in range(1, 3):
+                clips.extend(get_clips_by_stride(stride=stride, frames_list=all_frames, sequence_size=10))
+    return clips
+
+def get_model(reload_model=True):
+    """
+    Parameters
+    ----------
+    reload_model : bool
+        Load saved model or retrain it
+    """
+    if not reload_model:
+        return load_model(test_dataset_path,custom_objects={'LayerNormalization': LayerNormalization})
+    training_set = get_training_set()
+    training_set = np.array(training_set)
+    seq = Sequential()
+    seq.add(TimeDistributed(Conv2D(128, (11, 11), strides=4, padding="same"), batch_input_shape=(None, 10, 256, 256, 1)))
+    seq.add(LayerNormalization())
+    seq.add(TimeDistributed(Conv2D(64, (5, 5), strides=2, padding="same")))
+    seq.add(LayerNormalization())
+    # # # # #
+    seq.add(ConvLSTM2D(64, (3, 3), padding="same", return_sequences=True))
+    seq.add(LayerNormalization())
+    seq.add(ConvLSTM2D(32, (3, 3), padding="same", return_sequences=True))
+    seq.add(LayerNormalization())
+    seq.add(ConvLSTM2D(64, (3, 3), padding="same", return_sequences=True))
+    seq.add(LayerNormalization())
+    # # # # #
+    seq.add(TimeDistributed(Conv2DTranspose(64, (5, 5), strides=2, padding="same")))
+    seq.add(LayerNormalization())
+    seq.add(TimeDistributed(Conv2DTranspose(128, (11, 11), strides=4, padding="same")))
+    seq.add(LayerNormalization())
+    seq.add(TimeDistributed(Conv2D(1, (11, 11), activation="sigmoid", padding="same")))
+    print(seq.summary())
+    seq.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=1e-4, decay=1e-5, epsilon=1e-6))
+    seq.fit(training_set, training_set,
+            batch_size= BATCH_SIZE, epochs= EPOCHS, shuffle=False)
+    seq.save(test_dataset_path)
+    return seq
+
+
+def get_single_test():
+    sz = 200
+    test = np.zeros(shape=(sz, 256, 256, 1))
+    cnt = 0
+    for f in sorted(listdir(test_dataset_path)):
+        if str(join(test_dataset_path, f))[-3:] == "tif":
+            img = Image.open(join(test_dataset_path, f)).resize((256, 256))
+            img = np.array(img, dtype=np.float32) / 256.0
+            test[cnt, :, :, 0] = img
+            cnt = cnt + 1
+    return test
+
+def evaluate():
+    model = get_model(True)
+    print("got model")
+    test = get_single_test()
+    print("got test")
+    sz = test.shape[0] - 10
+    sequences = np.zeros((sz, 10, 256, 256, 1))
+    for i in range(0, sz):
+        clip = np.zeros((10, 256, 256, 1))
+        for j in range(0, 10):
+            clip[j] = test[i + j, :, :, :]
+        sequences[i] = clip
+
+    reconstructed_sequences = model.predict(sequences,batch_size=4)
+    sequences_reconstruction_cost = np.array([np.linalg.norm(np.subtract(sequences[i],reconstructed_sequences[i])) for i in range(0,sz)])
+    sa = (sequences_reconstruction_cost - np.min(sequences_reconstruction_cost)) / np.max(sequences_reconstruction_cost)
+    sr = 1.0 - sa
+
+    plt.plot(sr)
+    plt.ylabel('regularity score Sr(t)')
+    plt.xlabel('frame t')
+    plt.show()
+
