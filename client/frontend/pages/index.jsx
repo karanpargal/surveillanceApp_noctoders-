5de310f6df909acbcf8745a18c12773c04b77e2b
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-09-04 13:24:29 +0530

    :construction: added basic functionality for integration

--- client/frontend/pages/index.jsx
+++ client/frontend/pages/index.jsx
@@ -1,8 +1,36 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+import about from "../components/home/about";
+import { useState } from "react";
+
+const Home = () => {
+  const [file, setFile] = useState();
+  const [filename, setFileName] = useState("Choose File");
+  const [prediction, setPrediction] = useState(null);
+
+  const onChange = (e) => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+    console.log(file);
+
+    fetch("http://localhost:5000/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPrediction(data);
+      });
+  };
+
   return (
     <div>
       <Head>
@@ -63,6 +91,29 @@ const Home: NextPage = () => {
         <section>
           <div className="p-10 text-2xl">About the Project</div>
           <div></div>
+        </section>
+
+        <section>
+          <div className="p-10 text-2xl">Working</div>
+          <form onSubmit={onSubmit}>
+            <div className="custom-file">
+              <input
+                type="file"
+                className="custom-file-input"
+                id="customFile"
+                onChange={onChange}
+              />
+              <label className="custom-file-label" htmlFor="customFile">
+                {filename}
+              </label>
+            </div>
+            <input
+              type="submit"
+              value="Upload"
+              className="btn btn-primary btm-block mt-4"
+            />
+          </form>
+          {prediction && <h1>{prediction.message}</h1>}
         </section>
 
         <section>
